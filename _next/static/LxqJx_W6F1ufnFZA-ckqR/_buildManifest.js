a574528694fe648561fe70ce519263bd4f8fdc31
Vani Awasthi <awasthivani95@gmail.com>
2026-05-09 16:45:59 +0530

    Updates

---
+++ _next/static/LxqJx_W6F1ufnFZA-ckqR/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Mothersday//_next/:path+",
+        "destination": "/Mothersday/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
